4fa63197948d3d76bc207d4e7f6bac0a4f2e332e
Remco de Man <gitlab@remcodeman.nl>
2020-06-29 12:11:53 +0200

    Make script more fool-proof. Add basic documentation.

--- gen_patches.sh
+++ gen_patches.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd "${DIR}"
+
 while read FILE; do
   [[ -f "${FILE}" ]] || continue
   [[ -f "phabricator/${FILE}" ]] || continue
   diff -Naur "phabricator/${FILE}" "${FILE}" > "${FILE}.patch"
 done < <(cat files)
+
+popd
--- reapply_patches.sh
+++ reapply_patches.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd "${DIR}"
+
 while read FILE; do
   [[ -f "${FILE}.patch" ]] || continue
   [[ -f "phabricator/${FILE}" ]] || continue
   [[ -f "${FILE}" ]] && rm -f "${FILE}"
   cp "phabricator/${FILE}" "${FILE}"
-  patch < "${FILE}.patch"
+  patch -p 0 < "${FILE}.patch"
 done < <(cat files)
+
+popd
